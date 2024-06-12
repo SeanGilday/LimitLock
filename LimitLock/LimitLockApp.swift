08a0fda507f4a6ad43e9f2b188cbc6d750c5f4d2
Sean Gilday <spgilday19@gmail.com>
2024-06-12 18:08:05 -0400

    Initial Commit

---
+++ LimitLock/LimitLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimitLockApp.swift
+//  LimitLock
+//
+//  Created by Sean Gilday on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LimitLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
