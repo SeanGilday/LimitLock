4998f6d92ef4ad6e9da6d274fa48cf51faf7659d
Sean Gilday <spgilday19@gmail.com>
2024-06-12 18:21:25 -0400

    Test 2

--- LimitLock/ContentView.swift
+++ LimitLock/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("My name is Sean")
+            Text("LimitLock")
         }
         .padding()
     }
