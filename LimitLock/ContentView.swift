f558e7257820bf890275b0975480ccbd720aa56b
Sean Gilday <spgilday19@gmail.com>
2024-06-13 19:43:56 -0400

    Added ability to tell if current app is active, inactive, or in background

--- LimitLock/ContentView.swift
+++ LimitLock/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = "The best app."
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("LimitLock")
-        }
-        .padding()
+        Text(text)
+            .font(.system(size: 30))
+            .bold()
+            .onChange(of: scenePhase) {
+                if scenePhase == .active {
+                    self.text = "Active"
+                    print("App is active")
+                }   
+                else if scenePhase == .inactive {
+                    self.text = "Inactive"
+                    print("App is inactive")
+                }   
+                else if scenePhase == .background {
+                    print("App is in background")
+                }
+            }
     }
 }
 
